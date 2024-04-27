5ca1614db112407454f9affbc60671918231ee24
qmze <6255499+qmze@users.noreply.github.com>
2024-04-27 09:04:29 +0100

    Update capture_traffic.bat

--- capture_traffic.bat
+++ capture_traffic.bat
@@ -1,11 +1,2 @@
 @echo off
-cls
-echo on
-echo.
-echo   ______ _____ ________ ____  
-echo  / ____//     \___   // __ \ 
-echo < <_|  |  Y Y  \/    /\  ___/ 
-echo   \__   |__|_|  /_____ \\___  >
-echo      |__|     \/      \/    \/ 
-echo.
-pause
+start cmd
